d146137067a91738dd6090b79e046a2542f24277
BuildTools <unconfigured@null.spigotmc.org>
2020-02-21 15:51:17 -0600

    JEI integration and new info pages

---
+++ src/main/java/com/kwpugh/powder_power/util/JEIDescriptions.java
@@ -0,0 +1,46 @@
+package com.kwpugh.powder_power.util;
+
+import com.kwpugh.powder_power.PowderPower;
+import com.kwpugh.powder_power.lists.ItemList;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+
+@JeiPlugin
+public class JEIDescriptions implements IModPlugin
+{
+	static ResourceLocation plugin = new ResourceLocation(PowderPower.modid, "plugin");
+
+	@Override
+	public ResourceLocation getPluginUid()
+	{
+		return plugin;
+	}
+
+	@Override
+	public void registerRecipes(IRecipeRegistration registry)
+	{
+		registry.addIngredientInfo(new ItemStack(ItemList.hammer_alchemist), VanillaTypes.ITEM, "item.powder_power.hammer_alchemist.line3",
+				" ",
+				"item.powder_power.hammer_alchemist.line4",
+				" ",
+				"item.powder_power.hammer_alchemist.line5");
+		
+		registry.addIngredientInfo(new ItemStack(ItemList.wand_alchemist), VanillaTypes.ITEM, "item.powder_power.wand_alchemist.line3", 
+				" ",
+				"item.powder_power.wand_alchemist.line4",
+				"item.powder_power.wand_alchemist.line5",
+				"item.powder_power.wand_alchemist.line6",
+				"item.powder_power.wand_alchemist.line7",
+				"item.powder_power.wand_alchemist.line8",
+				"item.powder_power.wand_alchemist.line9",
+				"item.powder_power.wand_alchemist.line10",
+				"item.powder_power.wand_alchemist.line11",
+				"item.powder_power.wand_alchemist.line12",
+				"item.powder_power.wand_alchemist.line13");
+	}
+}
